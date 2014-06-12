51241d6257ffb6144ca2e5b965ae3c7a210491ea
Yuri Feldman <arrayjam@tbqh.net>
2014-06-13 01:51:19 +0700

    Composable

--- src/sidebar.js
+++ src/sidebar.js
@@ -44,20 +44,32 @@ var SidebarLayerList = React.createClass({
   },
 
   renameLayer: function(index, newName) {
-    this.setState({ renamingSelectedLayer: false });
     this.props.layers[index].name = newName;
+    this.setState({ renamingSelectedLayer: false });
   },
 
   render: function() {
     var layers = this.props.layers.map(function(layer, i) {
-      return <SidebarLayer
-        key={"layer-" + i}
-        name={layer.name}
-        selectLayer={this.selectLayer.bind(this, i)}
-        selected={this.state.selectedLayer === layer}
-        startRenamingLayer={this.startRenamingLayer}
-        renaming={this.state.selectedLayer === layer && this.state.renamingSelectedLayer}
-        renameLayer={this.renameLayer.bind(this, i)} />;
+      var selected = this.state.selectedLayer === layer,
+          renaming = selected && this.state.renamingSelectedLayer,
+          key = "layer-" + i;
+
+      if (renaming) {
+        return <RenamingSidebarLayer
+          key={key}
+          initialName={layer.name}
+          renameLayer={this.renameLayer.bind(this, i)} />
+      } else if (selected) {
+        return <SelectedSidebarLayer
+          key={key}
+          name={layer.name}
+          startRenamingLayer={this.startRenamingLayer} />;
+      } else {
+        return <SidebarLayer
+          key={key}
+          name={layer.name}
+          selectLayer={this.selectLayer.bind(this, i)} />;
+      }
     }, this);
     return (
       <div className="sidebarLayerList">
@@ -69,44 +81,68 @@ var SidebarLayerList = React.createClass({
 });
 
 var SidebarLayer = React.createClass({
-  onKey: function(event) {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      this.props.renameLayer(this.refs.input.getDOMNode().value);
-    }
-  },
+  render: function() {
+    return (
+      <div className="sidebarLayer" onClick={this.props.selectLayer}>
+        {this.props.name}
+      </div>
+    );
+  }
+});
 
+var SelectedSidebarLayer = React.createClass({
+  render: function() {
+    return (
+      <div className="sidebarLayer selected" onClick={this.props.startRenamingLayer}>
+        {this.props.name}
+      </div>
+    );
+  }
+});
+
+var RenamingSidebarLayer = React.createClass({
   getInitialState: function() {
-    return { name: this.props.name };
+    return { name: this.props.initialName };
   },
 
   handleChange: function(event) {
     this.setState({ name: event.target.value });
   },
 
-  render: function() {
-    var classes = cx({
-      "sidebarLayer": true,
-      "selected": this.props.selected
-    });
-
-    if (this.props.renaming) {
-      return (
-        <input type="text" onKeyPress={this.onKey} onChange={this.handleChange} ref="input" value={this.state.name}/>
-      );
-    } else if (this.props.selected) {
-      return (
-        <div className={classes} onClick={this.props.startRenamingLayer}>
-          {this.props.name}
-        </div>
-      );
+  handleOutsideClick: function(event) {
+    if (event.target === this.refs.input.getDOMNode()) {
+      event.preventDefault();
+      return false;
     } else {
-      return (
-        <div className={classes} onClick={this.props.selectLayer}>
-          {this.props.name}
-        </div>
-      );
+      this.renameLayer();
     }
+  },
+
+  componentDidMount: function() {
+    this.refs.input.getDOMNode().select();
+    document.addEventListener("click", this.handleOutsideClick);
+  },
+
+  componentWillUnmount: function() {
+    document.removeEventListener("click", this.handleOutsideClick);
+  },
+
+  renameLayer: function() {
+    var val = this.refs.input.getDOMNode().value;
+    this.props.renameLayer(val);
+  },
+
+  handleKeyPress: function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      this.renameLayer();
+    }
+  },
+
+  render: function() {
+    return (
+      <input type="text" value={this.state.name} onChange={this.handleChange} onKeyPress={this.handleKeyPress} ref="input"/>
+    );
   }
 });
 
